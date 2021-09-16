0b7d0c6fc8187bc19cbab57d777e992a78f3ebcd
Koshik Mahapatra <kmahapatra@Koshiks-MacBook-Pro.local>
2021-09-16 18:13:08 -0500

    early server commit

--- server.py
+++ server.py
@@ -1,7 +1,34 @@
 import socket
 import sys
+import threading
+import struct
+
 
 port = sys.argv[1]
-print(port)
+print("waiting on port", port, "...")
+
+BUFSIZE = 1028
+
+def listeningToKeyboard():
+    for line in sys.stdin:
+        #implements ctrl d and ctrl c always works because of the interrupt signal. Does q need to exit program?
+        if line == '^D' or len(line) == 0:
+            exit()
+        
+def listeningToSocket(server_port):
+   server_port = int(server_port)
+   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   s.bind(('', server_port))
+   print("Server is ready to listen")
+   while True:
+       data, addr = s.recvfrom(BUFSIZE)
+
+
+
+main_thread = threading.Thread(name = 'main_thread', target = listeningToKeyboard, daemon = None)
+main_thread.start()
+
+T1 = threading.Thread(name = 'thread_one', target = listeningToSocket, args = (port,), daemon = True)
+T1.start()
 
 
